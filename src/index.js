fe30ed2b8f2c81d549d65f62ac913ff14bf824bd
smile930307 <ImamEugenaU2K@gmail.com>
2022-08-18 08:01:53 +0900

    fix error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { ErrorBoundary } from './error/index';
+import { ErrorBoundary } from 'error/index';
 import store from 'app/Store/index';
 import App from './App';
 import 'styles/app.scss';
